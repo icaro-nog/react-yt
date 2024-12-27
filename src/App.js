1660d78031cc88a5dc7394983eb48f68ec1c97ae
icaro nogueira <icaronogueira@deltaglobal.com.br>
2024-12-27 08:32:08 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo!</h1>
+      <p>Primeiro app React</p>
+    </div>
+  );
+}
+
+export default App;
